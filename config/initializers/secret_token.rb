2f708a210d1a2a857068cd0080b8f9294cc23c17
alu0100600582 <alu0100600582@ull.edu.es>
2014-02-18 11:01:15 +0000

    Inicializando repositorio prac2 Django

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prac2::Application.config.secret_key_base = '04ca1b8437c5b2cdc0573026083f10fcd86ce3d73b9ba184a340df7f1882b4fec17e3548e57d9e08bc5077f667c3f34f04bb40ad8475c6b8ed8091852af10ec6'
